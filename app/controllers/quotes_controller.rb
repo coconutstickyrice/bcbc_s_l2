8723e5d65bd7d783744beea79a8533f429b3c31b
coconutstickyrice <sunfare@gmail.com>
2019-03-12 20:13:21 +0000

    Lesson 13 updates

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,10 +1,25 @@
 class QuotesController < ApplicationController
+
+# display a random quote stored in database   
   def index
       @quote = Quote.order("RANDOM()").first
   end
 
+# get new quote from user via form 
   def new
     @quote = Quote.new
+  end
+
+# sends quote (saying & author) to database 
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
   end
 
 end
